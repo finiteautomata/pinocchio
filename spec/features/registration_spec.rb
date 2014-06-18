7238f194496de9dd3de2851b468de0aae4103804
jmperez <jmperez.85@gmail.com>
2014-06-18 00:11:11 -0300

    Add acceptance test.

---
+++ spec/features/registration_spec.rb
@@ -0,0 +1,19 @@
+describe "Registration" do
+
+  let(:name) { "Juan Pérez" }
+  let(:email) { "juanperez@example.com" }
+  let(:password) { "password" }
+
+  it "should be able to sign up succesfully" do
+    visit new_user_path
+
+    fill_in 'Nombre', with: name
+    fill_in 'Email', with: email
+    fill_in 'Clave', with: password
+    fill_in 'Repita su clave', with: password
+
+    click_button 'Registrarme'
+
+    expect(page).to have_content("Hola, #{name}")
+  end
+end
